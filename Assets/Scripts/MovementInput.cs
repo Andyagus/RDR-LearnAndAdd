0df3c03cacd667a3a6fd020e2bb8148eb9548f20
Andy Agus <Redagus@gmail.com>
2022-04-07 15:41:52 -0400

    Initial Commit

---
+++ Assets/Scripts/MovementInput.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementInput : MonoBehaviour
+{
+
+    public float Velocity;
+    public float InputX;
+    public float InputZ;
+    public Vector3 desiredMoveDirection;
+    public bool blockRotationPlayer;
+    public float desiredRotationSpeed = 0.1f;
+    public Animator anim;
+    public float Speed;
+    public float allowPlayerRotation = 0.1f;
+    public Camera cam;
+    public CharacterController controller;
+    public bool isGrounded;
+    private float verticalVel;
+    private Vector3 moveVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputMagnitude();
+        isGrounded = controller.isGrounded;
+
+        if(isGrounded == true)
+        {
+            verticalVel -= 0;
+        }
+        else
+        {
+            verticalVel -= 2;
+        }
+
+        moveVector = new Vector3(0, verticalVel, 0);
+        controller.Move(moveVector);
+    }
+
+    void PlayerMoveAndRotation()
+    {
+        InputX = Input.GetAxis("Horizontal");
+        InputZ = Input.GetAxis("Vertical");
+
+        var forward = cam.transform.forward;
+        var right = cam.transform.right;
+
+        forward.y = right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        desiredMoveDirection = forward * InputZ + right * InputX;
+
+        if (!blockRotationPlayer)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), desiredRotationSpeed);
+            controller.Move(desiredMoveDirection * Time.deltaTime * Velocity);
+        }
+    }
+
+    public void LookAt()
+    {
+
+    }
+
+    public void RotateToCamera()
+    {
+
+    }
+
+    void InputMagnitude()
+    {
+        InputX = Input.GetAxis("Horizontal");
+        InputZ = Input.GetAxis("Vertical");
+
+        Speed = new Vector2(InputX, InputZ).sqrMagnitude;
+
+        if(Speed > allowPlayerRotation)
+        {
+            PlayerMoveAndRotation();
+        }
+        
+    }
+}
---
+++ Assets/Scripts/ShooterController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShooterController : MonoBehaviour
+{
+    private MovementInput input;
+    private Animator anim;
+
+    [Space]
+
+
+
+    [Header("Gun")]
+    public Transform gun;
+    private Vector3 gunIdlePosition;
+    private Vector3 gunIdleRotation;
+    private Vector3 gunAimPosition = new Vector3(0.2401146f, .006083928f, -0.1040046f);
+    private Vector3 gunAimRotation = new Vector3(-6.622f, 97.47501f, 94.774f);
+
+
+    void Start()
+    {
+        input = GetComponent<MovementInput>();
+        anim = GetComponent<Animator>();
+
+        gunIdlePosition = gun.localPosition;
+        gunIdleRotation = gun.localEulerAngles;
+
+        anim.SetBool("aiming", true);
+        gun.localPosition = gunAimPosition;
+        gun.localEulerAngles = gunAimRotation;
+    }
+
+    void Update()
+    {
+        anim.SetFloat("Speed", input.Speed);
+
+    }
+}
