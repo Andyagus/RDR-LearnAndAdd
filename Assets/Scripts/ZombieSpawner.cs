a98cc60878474484ab306c42de98779035035f84
Andy Agus <Redagus@gmail.com>
2022-04-19 18:46:54 -0400

    work on spawn

---
+++ Assets/Scripts/ZombieSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour
+{
+
+    public GameObject zombiePrefab;
+    public bool spawnZombie = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnZombies());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public IEnumerator SpawnZombies()
+    {
+        while (spawnZombie)
+        {
+            yield return new WaitForSeconds(1);
+            gameObject.transform.position = new Vector3(Random.Range(0, 100), 0, 0);
+            Instantiate(zombiePrefab, gameObject.transform);
+        }
+    }
+}
